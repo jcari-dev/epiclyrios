9fb875047aa28578a0342037d9a6c33d9da239c3
hayleytaylor29 <hft929@gmail.com>
2021-06-15 22:49:34 -0400

    created function for lyrics api, started setting up backend

--- lyrios-backend/server.js
+++ lyrios-backend/server.js
@@ -1,1 +1,11 @@
-console.log("let's have a kiki")
+console.log('this works')
+
+const express = require('express');
+const cors = require('cors');
+
+//middleware
+app.use(cors());
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- lyrios-frontend/src/App.js
+++ lyrios-frontend/src/App.js
@@ -1,11 +1,13 @@
 
 import Search from './components/Search'
+import Lyrics from './components/Lyrics'
 function App() {
   const URL = "testing"
   return (
     <div className="App">
       <h1>Lets get started with the Lyrios App!</h1>
       <Search/>
+      <Lyrics/>
     </div>
   );
 }
