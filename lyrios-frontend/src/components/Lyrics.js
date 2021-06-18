d9a3526b1730ba09d93f6f1a3a629c05e7bbd6a6
hayleytaylor29 <hft929@gmail.com>
2021-06-18 19:27:19 -0400

    reworked api, got to display lyrics with artist and track hardcoded. Will pull in what other updates have been made to try and connect my calls to Michael's youTube call

--- lyrios-frontend/src/components/Lyrics.js
+++ lyrios-frontend/src/components/Lyrics.js
@@ -1,71 +1,47 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios'
 
 //need to require cors in server.js and app.use middleware
+function Lyrics (props) {
+const baseURL = "https://api.happi.dev/v1/music?";
+const q = "&q=ariana%20grande%20needy";
+const hasLyrics = "&lyrics=1";
+const limit = "&limit=";
+const type = "&type=";
+const apiKey = "&apikey=6d400baq5E0tR7e8ItaBRyijAyJVpD9qLDYxcli0AwBHLoMayAPtZaNr";
+const URL = `${baseURL}${q}${limit}${apiKey}${type}${hasLyrics}`;
 
-
-const Data = (props) => {
-
-const baseURL = "https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=json&callback=callback";
-const searchTrack = "&q_track=supermodel";
-const searchArtist = "&q_artist=sza"
-const apiKey = "&apikey=9901cd7566e5b6558969d80850b2fc99";
-const URL = `${baseURL}${searchTrack}${searchArtist}${apiKey}`;
-//state to hold data
-const [lyrics, setLyrics] = React.useState('null')
-//function to make api call
-const getLyrics = async () => {
-    console.log(URL)
-    //make api call to get response
-    const response = await fetch(URL);
-    //turn response into js object
-    const data = await response.json();
-    //set lyrics state to data
-    setLyrics(data)
-    console.log(data)
-}
-//get lyrics only after page is rendered
-React.useEffect(() => {
-    getLyrics();
-});
-//return data and render to page
-const loaded = () => {
-    return (
+    //function to make api call
+    const getData = async (searchTerm) => {
+        // console.log(URL)
+        //make the call to get a response
+        const response = await Axios.get(
+            URL
+        )
+        //set lyrics state to response
+        getLyricsData(response.data.result[0].api_lyrics)
+        // console.log(response)
+       return 'response'
+    }
+        
+    const [lyricsData, setLyricsData] = React.useState(null)
+    const getLyricsData = async (data) => {
+        const response = await Axios.get(
+            data +'?'+apiKey
+        )
+        setLyricsData(response.data.result.lyrics)
+        return 'response'
+    }
+    //get only after page is rendered
+    useEffect(()=>{
+        getData('song')
+    }, [])
+    //return statement from the call to render to page
+    return(
         <div>
-            {/* <h2>{lyrics.body.lyrics.lyrics_body}</h2> */}
-        </div>
+            <h3>{lyricsData ? <>  {lyricsData}  </> : 'No Lyrics data was gathered'}</h3>
+         </div>
     )
 }
-//if data arrives, return lyrics, if not return the second return statement
-const loading = () => {
-    return <h1>Loading...</h1>
-};
-    return lyrics ? loaded() : loading();
-}
 
-export default Data;
-
-// function Lyrics (props) {
-//     const [lyricsData, setLyricsData] = React.useState(null)
-//     const getLyrics = async (searchTerm) => {
-//         const response = await Axios.get(
-//             URL
-//         )
-//         setLyricsData(response)
-//         console.log(response)
-//        return 'response'
-//     }
-//     useEffect(()=>{
-//         getLyrics('song')
-//     },[])
-//     console.log(lyricsData)
-//     return(
-//         <div>
-//             {/* <h2>{<Lyrics getLyrics = {lyricsData.body.lyrics.lyrics_body} />}</h2> */}
-//             <h2>{lyricsData ? <>  {lyricsData.body.lyrics.lyrics_body}  </> : 'No Lyrics data was gathered'}</h2>
-//          </div>
-      
-//     )
-
-// }
-
+export default Lyrics;
